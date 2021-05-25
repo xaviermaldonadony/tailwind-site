22ccbc941637c5b7d45015c5ea68c3617ec95474
Xavi <xaviermaldonadony@hotmail.com>
2021-05-25 14:26:48 -0400

    first commit, porject set up

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	purge: ['./*.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				orange: colors.orange,
+				teal: colors.teal,
+				'light-blue': colors.lightBlue,
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
