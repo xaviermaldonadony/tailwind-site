8b95ef543616289caeb70c280bfed547f013c797
Xavi <xaviermaldonadony@hotmail.com>
2021-05-28 22:16:27 -0400

    update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,9 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('tailwindcss'),
+		require('autoprefixer'),
+	],
 };
